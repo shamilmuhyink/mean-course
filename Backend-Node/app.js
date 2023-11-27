6c8c9813cd91e58166592e94660d840c843f7de4
Shamil Muhyin K <shamil.muhyin@ACTCORP.LOCAL>
2023-11-27 12:08:13 +0530

    Added MEAN diagram pdf

--- Backend-Node/app.js
+++ Backend-Node/app.js
@@ -3,30 +3,31 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use( "/api/posts",(req, res, next) => {
+app.post( "/api/posts",(req, res, next) => {
     const post = req.body;
     console.log(post);
     res.status('201').json({
         message: "Post added successfully"
     });
 });
-app.use("/api/posts", (req, res, next)=>{
-    const posts = [
-        {
-            id:"1",
-            title:"first element",
-            content:"from server"
-        },
-        {
-            id:"2",
-            title:"second element",
-            content:"from server"
-        }
-    ];
-    res.status(200).json({
-        message: "posts fetched successfully",
-        posts: posts
-    })
-});
+
+// app.use("/api/posts", (req, res, next)=>{
+//     const posts = [
+//         {
+//             id:"1",
+//             title:"first element",
+//             content:"from server"
+//         },
+//         {
+//             id:"2",
+//             title:"second element",
+//             content:"from server"
+//         }
+//     ];
+//     res.status(200).json({
+//         message: "posts fetched successfully",
+//         posts: posts
+//     })
+// });
 
 module.exports = app;
